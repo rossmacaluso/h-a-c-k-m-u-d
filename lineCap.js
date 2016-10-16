b4b607ca7fef6107373aa5a2dab88002597fcfe5
rossmacaluso <ross.macaluso@gmail.com>
2016-10-16 11:54:57 -0700

    update: remove old parseLines, lineCap has some comments, added tmp primeNum generator

--- lineCap.js
+++ lineCap.js
@@ -2,13 +2,12 @@
 function (context, args) {
   var
     target = args.t,
-    n = target.name.split('.')[0],
+    //n = target.name.split('.')[0], //return script name
     k1 = args.k,
     v1 = args.v,
     o = #s.scripts.lib(),
-    //vstr = '"' + v1 + '"',
+    //vstr = '"' + v1 + '"', //test to see if needed to add quotes to pass key:value
     a = {},
-    //l = #s.scripts.lib
     cc = args.cc,
     m = "";
 
@@ -16,19 +15,19 @@ function (context, args) {
   var tx = target.call( a );
   var st = tx.toString().split( "\n" );
 
-  //#s.chats.tell( { to:context.caller, msg: v1 } );
+  //#s.chats.tell( { to:context.caller, msg: v1 } ); //hacked debugging...
   if ( st.length > cc ) {
     for ( var ln = 0; ln <= cc; ln++ ){
-      o.log( st[ln] + '\n' );
+      o.log( st[ln] );
     }
   }
   else {
-    o.log ("input string length not greater than requested resulting string, printing input string:\n\n" + tx );
+    o.log ("input string length not greater than requested resulting string, printing input string: " + tx ); // return original string if it's already < requested len
   }
 
   return {
-      o.ok(),
-      output: o.get_log()
-      //debug: o
+      o.ok(), //success?
+      output: o.get_log() // output return
+      //debug: o // debug return
   };
 }
--- parseLines.js
+++
@@ -1,3 +0,0 @@
-#attempt at creating a script to parse incremental line by line text from a readout
-
-function { }
