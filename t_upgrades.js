ee10a782a5052adbc00eddf7da4fe08a6430c963
rossmacaluso <ross.macaluso@gmail.com>
2016-10-24 02:02:02 -0700

    t_upgrades removed for loops of upgrades; trying to keep for loop for input arrays

--- t_upgrades.js
+++ t_upgrades.js
@@ -4,38 +4,33 @@ function (context, a) { // i:1, to:"username", confirm:true
   // i = index can be a number, list or ALL|all and will transfer the requested upgrades to specified user
   var
     o = #s.scripts.lib(),
-    i = a.i,
-    to = a.to,
+    upg_i = a.i,
+    upgr_to = a.to,
     c = a.confirm,
     i_re = /all/gmi,
     upgrade = "",
     result = "",
     loaded = true,
+    k = {},
     su = #s.sys.upgrades();
 
   o.log( "System Upgrades: " + su.length );
-  //o.log( "indexList Length: " + typeof(i) );
+  o.log( "indexList Length: " + upg_i.length );
 
-  if ( i_re.test( i ) == true ) {
-    if ( to != undefined ) {
-      o.log( "Transfering All Upgrades! To: " + to );
-      for ( var sui = 0; sui < su.length; sui++ ) {
-        upgrade = #s.sys.upgrades( { info:sui } );
-      }
+  if ( i_re.test( upg_i ) == true ) {
+    if ( upgr_to != undefined ) {
+      o.log( "Transfering All Upgrades! To: " + upgr_to );
       if ( !( "confirm" in a ) ) {
         o.log( "add {confirm:true} To Confirm" );
       }
       else {
         if ( a.confirm == true ) {
+          upgrade = #s.sys.upgrades( { info:upg_i } );
           loaded = upgrade["loaded"];
-          o.log( "Loaded: " + loaded );
-          if ( loaded == true ){
-            #s.sys.upgrades( { unload:sui } );
-          }
-          result = #s.sys.xfer_upgrades_to( { i:sui, to:to } );
-          if ( "Failure" in result ){
-            o.log( "Can't Transfer Upgrade: " + result );
-          }
+          #s.sys.upgrades( { unload:upg_i } );
+          k.i = upg_i;
+          k.to = upgr_to;
+          #s.sys.xfer_upgrade_to( k );
         }
         else {
           o.log( "confirm != true")
@@ -47,28 +42,30 @@ function (context, a) { // i:1, to:"username", confirm:true
     }
   }
 
-  else if ( i.length > 1 ) {
-    if ( to != undefined ) {
-      for ( var ui = 0; ui < i.length; ui++ ) {
-        upgrade = #s.sys.upgrades( { info:ui } );
-        o.log( "Transfering Upgrade: " + upgrade["name"] + " To: " + to );
-        if ( !( "confirm" in a ) ) {
-          o.log( "add {confirm:true} To Confirm" );
-        }
-        else {
-          if ( a.confirm == true ) {
+  else if ( upg_i.length >= 1 ) {
+    if ( upgr_to != undefined ) {
+      for ( var ai = 0; ai < upg_i.length; ai++ ) {
+        upgrade = #s.sys.upgrades( { info:upg_i[ai] } );
+        o.log( "Transfering Upgrade: " + upgrade["name"] + " To: " + upgr_to );
+      }
+      if ( !( "confirm" in a ) ) {
+        o.log( "add {confirm:true} To Confirm" );
+      }
+      else {
+        if ( a.confirm == true ) {
+          for ( var bi = 0; bi < upg_i.length; bi++ ) {
+            upgrade = #s.sys.upgrades( { info:upg_i[bi] } );
             loaded = upgrade["loaded"];
             if ( loaded == true ) {
-              #s.sys.upgrades( { unload:ui } );
-            }
-            result = #s.sys.xfer_upgrades_to( { i:ui, to:to } );
-            if ( "Failure" in result ){
-              o.log( "Can't Transfer Upgrade: " + result );
+              #s.sys.upgrades( { unload:upg_i[bi] } );
             }
           }
-          else {
-            o.log( "confirm != true")
-          }
+          k.i = upg_i;
+          k.to = upgr_to;
+          #s.sys.xfer_upgrade_to( k );
+        }
+        else {
+          o.log( "confirm != true")
         }
       }
     }
@@ -77,10 +74,10 @@ function (context, a) { // i:1, to:"username", confirm:true
     }
   }
 
-  else if ( typeof(i) == "number" ) {
-    if ( to != undefined ) {
-      upgrade = #s.sys.upgrades( { info:i } );
-      o.log( "Transfering Upgrade: " + upgrade["name"] + " To: " + to );
+  else if ( typeof(upg_i) == "number" ) {
+    if ( upgr_to != undefined ) {
+      upgrade = #s.sys.upgrades( { info:upg_i } );
+      o.log( "Transfering Upgrade: " + upgrade["name"] + " To: " + upgr_to );
       if ( !( "confirm" in a ) ) {
         o.log( "add {confirm:true} To Confirm" );
       }
@@ -89,12 +86,11 @@ function (context, a) { // i:1, to:"username", confirm:true
           loaded = upgrade["loaded"];
           o.log( "Loaded: " + loaded );
           if ( loaded == true ) {
-            #s.sys.upgrades( { unload:i } );
+            #s.sys.upgrades( { unload:upg_i } );
           }
-          result = #s.sys.xfer_upgrades_to( { i:i, to:to } );
-          if ( "Failure" in result ){
-            o.log( "Can't Transfer Upgrade: " + result );
-          }
+          k.i = upg_i;
+          k.to = upgr_to;
+          #s.sys.xfer_upgrade_to( k );
         }
         else {
           o.log( "confirm != true")
